93f3460fe725259c6bb36c19f50c41daee0026ba
Yulia Tamara Kluev <ytkluev@uwaterloo.ca>
2025-03-08 17:34:41 -0500

    1.11: unicafe step 6 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,7 +2,56 @@ import { useState } from 'react'
 
 const Header = (props) => <h1>{props.text}</h1>
 
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const StatisticLine = ({text, value}) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
+
+const Statistics = ({stats}) => {
+
+  if (stats.all == 0) {
+    return (
+      <>
+      <Header text="statistics"></Header>
+      <p>No feedback given</p>
+      </>
+    )
+  }
+  return (
+    <>
+      <Header text="statistics"></Header>
+
+      <table>
+        <thead>
+          <StatisticLine text="good" value ={stats.good} />
+          <StatisticLine text="neutral" value ={stats.neutral} />
+          <StatisticLine text="bad" value ={stats.bad} />
+          <StatisticLine text="all" value ={stats.all} />
+          <StatisticLine text="average" value ={stats.average} />
+          <StatisticLine text="percent" value ={stats.percent + " %"} />
+        </thead>
+      </table>
+    </>
+  )
+  
+  
+}
+
 const App = () => {
+  const new_stats = {
+    "good" : 0,
+    "neutral" : 0,
+    "bad" : 0,
+    "all" : 0,
+    "average": 0,
+    "percent": 0
+  }
+
+
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -11,6 +60,14 @@ const App = () => {
   const [average, setAverage] = useState(0)
   const [percent, setPercent] = useState(0)
 
+  const stats = {
+    "good" : good,
+    "neutral" : neutral,
+    "bad" : bad,
+    "all" : all,
+    "average": average,
+    "percent": percent
+  }
 
   const handleGoodClick = () => {
     const updatedGood = good + 1
@@ -46,17 +103,10 @@ const App = () => {
   return (
     <div>
       <Header text="give feedback"></Header>
-      <button onClick={handleGoodClick}>good</button>
-      <button onClick={handleNeutralClick}>neutral</button>
-      <button onClick={handleBadClick}>bad</button>
-
-      <Header text="statistics"></Header>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {percent} %</p>
+      <Button onClick={handleGoodClick} text="good"/>
+      <Button onClick={handleNeutralClick} text="neutral"/>
+      <Button onClick={handleBadClick} text="bad"/>
+      <Statistics stats={stats}></Statistics>
     </div>
   )
 }
