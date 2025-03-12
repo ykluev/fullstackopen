fd7e72614233f78f46d385d77f058166c6c1b9c8
Yulia Tamara Kluev <ytkluev@uwaterloo.ca>
2025-03-12 15:50:20 -0400

    part2: courseinfo base code

---
+++ lessons/part2/Notes.md
@@ -0,0 +1,8 @@
+# Part 2 Notes
+
+
+## a.
+
+- `map` always creates a new array using a mapping function
+- antipattern: array indices as keys
+  - this can work initially but cause problems as we add new items to the array and the indices shift
---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,53 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => (
+  <div>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p>Number of exercises {props.total}</p>
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        total={
+          course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises
+        }
+      />
+    </div>
+  )
+}
+
+export default App
