83d9e1e60ff1b18ca50ba2b2eeb0fcf4f1f950c0
Yulia Tamara Kluev <ytkluev@uwaterloo.ca>
2025-03-13 16:13:26 -0400

    2.11 phonebook json server completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ({name, number}) => (
   <p>{name} {number}</p>
@@ -51,12 +51,7 @@ const Persons = ({persons, searchFilter}) => {
 }
    
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchFilter, setNewFilter] = useState('')
@@ -96,6 +91,13 @@ const App = () => {
     number: {val: newNumber, handler: handleNumberChange}
   }
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })    
+  }, [])
 
   return (
     <div>
